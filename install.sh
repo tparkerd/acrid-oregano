a286d97556c507f6a9c8969990e74d4e7c04ee24
Tim Parker <tim.parkerd@gmail.com>
2018-10-08 17:18:53 -0500

    Update the references to version 9.5 instead of 9.4

---
+++ install.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# Candidate installation script for BaxDB (i.e., GWAS database)
+# Operating system: CentOS 7
+# RDBMS: PostgreSQL 9.5
+
+# Install the dependencies
+yum -y update
+yum -y install wget unzip gcc perl dos2unix
+yum -y install epel-release
+yum -y install https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos94-9.5-3.noarch.rpm
+yum -y install postgresql95 postgresql95-server postgresql95-contrib postgresql95-libs postgresql95-devel
+export PATH=/usr/pgsql-9.5/bin:$PATH
+postgresql95-setup initdb
+systemctl enable postgresql-9.5.service
+systemctl start postgresql-9.5.service
+
+# Run script that creates the TINYINT datatype and then create the tables
+./setup.sh
