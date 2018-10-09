a6e3de4f8a8e7ffe50ad218bb72c62c82a7380e9
Tim Parker <tim.parkerd@gmail.com>
2018-10-09 14:00:02 -0500

    Change version of postgresql back to 9.4

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@
 # RDBMS: PostgreSQL 9.5
 # NOTE: Make sure to run this under `root` user
 
+# Known issues: 
+# During ./setup.sh, the build throws the error that a parameter is unused (postgresql v9.5)
+
 # Install the dependencies
 yum -y update
 yum -y install wget unzip gcc perl dos2unix
 yum -y install epel-release
-yum -y install https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-3.noarch.rpm
-yum -y install postgresql95 postgresql95-server postgresql95-contrib postgresql95-libs postgresql95-devel
+yum -yinstall https://download.postgresql.org/pub/repos/yum/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-3.noarch.rpm
+yum -y install postgresql94 postgresql94-server postgresql94-contrib postgresql94-libs postgresql94-devel
 export PATH=/usr/pgsql-9.5/bin:$PATH
-postgresql95-setup initdb
+postgresql94-setup initdb
 systemctl enable postgresql-9.5.service
 systemctl start postgresql-9.5.service
 
