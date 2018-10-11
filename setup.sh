365ef7cedf2c57e42e070931d9150e37cc2ba913
Tim Parker <tim.parkerd@gmail.com>
2018-10-11 13:06:53 -0500

    Fixed mismatch in case of install directory

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@
 # Verify that pg_config is accessible to the user
 command -v pg_config > /dev/null 2>&1 || { printf "Command 'pg_config' is required but not found in path. Make sure PostgreSQL client tools are installed. Aborting.\n" 1>&2; exit 1; }
 
-PG_LIBDIR=$(pg_config --pkglibdir)
-PG_INSTALLDIR="$PG_LIBDIR/baxdb"
-mkdir -p -m 755 "$PG_INSTALLDIR" || { printf "Unable to create directory '$pg_installdir' for installation into PostgreSQL. Aborting.
+pg_libdir=$(pg_config --pkglibdir)
+pg_installdir="$pg_libdir/baxdb"
+mkdir -p -m 755 "$pg_installdir" || { printf "Unable to create directory '$pg_installdir' for installation into PostgreSQL. Aborting.
 \n" 1>&2; exit 1; }
 
 (
     cd ./c &&
     make &&
-    cp array_multi_index.so imputed_genotype.so summarize_variant.so "$PG_INSTALLDIR" &&
-    chmod -R 755 "$PG_INSTALLDIR"
+    cp array_multi_index.so imputed_genotype.so summarize_variant.so "$pg_installdir" &&
+    chmod -R 755 "$pg_installdir"
     printf "Created array_multi_index, imputed_genotype, and summarize_variant.\n"
 )
 
@@ -21,8 +21,8 @@ mkdir -p -m 755 "$PG_INSTALLDIR" || { printf "Unable to create directory '$pg_in
     cd ./lib/tinyint-0.1.1 &&
     make &&
     sed -i -e '1i\\\connect baxdb' -e 's|$libdir\/tinyint|$libdir/baxdb/tinyint|g' tinyint.sql &&
-    cp tinyint.so "$PG_INSTALLDIR" &&
-    chmod -R 755 "$PG_INSTALLDIR"
+    cp tinyint.so "$pg_installdir" &&
+    chmod -R 755 "$pg_installdir"
     cp tinyint.sql "$pg_installdir" # move the tinyint.sql into a location accessible by postgres user
     printf "Created TINYINT SQL and moved to $pg_installdir.\n"
 )
