16d93e3939906b93d9d5d169f9f6f00098a18b6c
Tim Parker <tim.parkerd@gmail.com>
2018-10-11 16:08:26 -0500

    Update version to 9.5 in order to support the insert statements in the python scripts

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@
 yum -y update
 yum -y install wget unzip gcc perl dos2unix
 yum -y install epel-release
-yum -y install https://download.postgresql.org/pub/repos/yum/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-3.noarch.rpm
-yum -y install postgresql94 postgresql94-server postgresql94-contrib postgresql94-libs postgresql94-devel
-export PATH=/usr/pgsql-9.4/bin:$PATH
-postgresql94-setup initdb
-systemctl enable postgresql-9.4.service
-systemctl start postgresql-9.4.service
+yum -y install https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-3.noarch.rpm
+yum -y install postgresql95 postgresql95-server postgresql95-contrib postgresql95-libs postgresql95-devel
+export PATH=/usr/pgsql-9.5/bin:$PATH
+postgresql95-setup initdb
+systemctl enable postgresql-9.5.service
+systemctl start postgresql-9.5.service
 
 # Run script that creates the TINYINT datatype and then create the tables
 ./setup.sh
