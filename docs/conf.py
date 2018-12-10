31e2029489ea449ff28b508b0629255a2b733dfc
Tim Parker <Tim.ParkerD@gmail.com>
2018-12-10 15:29:19 -0600

    Check if the cause of not autogen is due to formatting

--- docs/conf.py
+++ docs/conf.py
@@ -159,27 +159,27 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
-def run_apidoc(_):
-  ignore_paths = []
+# def run_apidoc(_):
+#   ignore_paths = []
 
-  argv = [
-    "-f",
-    "-T",
-    "-e",
-    "-M",
-    "-o", ".",
-    ".."
-  ] + ignore_paths
+#   argv = [
+#     "-f",
+#     "-T",
+#     "-e",
+#     "-M",
+#     "-o", ".",
+#     ".."
+#   ] + ignore_paths
 
-  try:
-    # Sphinx 1.7+
-    from sphinx.ext import apidoc
-    apidoc.main(argv)
-  except ImportError:
-    # Sphinx 1.6 (and earlier)
-    from sphinx import apidoc
-    argv.insert(0, apidoc.__file__)
-    apidoc.main(argv)
+#   try:
+#     # Sphinx 1.7+
+#     from sphinx.ext import apidoc
+#     apidoc.main(argv)
+#   except ImportError:
+#     # Sphinx 1.6 (and earlier)
+#     from sphinx import apidoc
+#     argv.insert(0, apidoc.__file__)
+#     apidoc.main(argv)
 
-def setup(app):
-  app.connect('builder-inited', run_apidoc)
+# def setup(app):
+#   app.connect('builder-inited', run_apidoc)
