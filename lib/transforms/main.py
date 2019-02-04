6d0b8335f13e4f37712d3ada8e415a7388af9065
Tim Parker <tparkerd@users.noreply.github.com>
2019-02-04 13:23:52 -0600

    Setaria Datafile Splitter (#27)
    * Reluctant commit, portion of VCF genotype data for Setaria
    Series creation of chromosome mappings and line numbers per target file
    are in progress and need to be assigned to overall dataframe of genotype
    data
    
    * Hot fix, include vcf input file argument
    
    * Reluctant commit, python parser of Setaria 012 triplets
    Testing to see if extracting each of the chromosome columns from
    the .012 file based on line numbers of chromosome instances in .pos
    file
    
    * Clean up, removed extra datasets, updated Setaria genotype data transformer (cut.py)
    Removed extra datasets meant for development
    Removed back up files and temporary .tmpdf file
    Included .012, .012.pos, and .012.indv in .gitignore

--- lib/transforms/main.py
+++ lib/transforms/main.py
@@ -80,6 +80,7 @@ def parseOptions():
   directory = './lib/transformer'
   transformers = list(set([ f[:-3] for f in os.listdir(directory) if not f.startswith('_') and f.endswith('.py') ]))
   parser.add_argument("-t", "--transformer", default = None, help = f"Name of the format transformer to use. List of available transformers: {transformers}")
+  parser.add_argument("--vcf_input", default = None, help = f"Path to the VCF file that contains genotype data for all chromosomes. Required for vcf_* transformers")
   parser.add_argument("--index", default = None, help = "NOT IMPLEMENTED. Name of the column for input")
   parser.add_argument("--debug", action = "store_true", help = "Enables --verbose and disables writes to disk")
   args = parser.parse_args()
