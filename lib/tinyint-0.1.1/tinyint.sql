b0e2cfb3b97977f44a035016edac3f3aa9afa375
Tim Parker <Tim.ParkerD@gmail.com>
2019-06-26 12:19:21 -0500

    Removed unnecessary connection lines in the tinyint SQL library

--- lib/tinyint-0.1.1/tinyint.sql
+++ lib/tinyint-0.1.1/tinyint.sql
@@ -1,9 +1,3 @@
-\connect baxdb
-\connect baxdb
-\connect baxdb
-\connect baxdb
-\connect baxdb
-\connect baxdb
 CREATE FUNCTION tinyint_in(cstring) RETURNS tinyint AS
 '$libdir/baxdb/tinyint'
 LANGUAGE c IMMUTABLE STRICT;
