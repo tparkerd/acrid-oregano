0a9f98e118b019e20520727b315763748e53a913
Tim Parker <Tim.ParkerD@gmail.com>
2020-08-19 13:19:57 -0500

    Update installation of development libraries for PSQL

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Installation script for BaxDB (i.e., GWAS database)
+# Installation script for pgwasdb (i.e., GWAS database)
 # Operating system: CentOS 7
 # RDBMS: PostgreSQL 9.6
 # NOTE: Make sure to run this under `root` user
@@ -30,7 +30,7 @@ if [ $psql_status -ne 0 ]; then
   if [ $psql_status -eq 4 ]; then
     dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
     dnf -qy module disable postgresql
-    dnf install -y postgresql96-server
+    dnf install -y postgresql96-server postgresql96-devel
     export PATH="/usr/pgsql-9.6/bin/:$PATH"
     postgresql96-setup initdb
     systemctl enable postgresql-9.6 || {
@@ -53,7 +53,7 @@ command -v pg_config >/dev/null 2>&1 || {
 
 # Compile TINYINT extention and install it into PostgreSQL
 pg_libdir=$(pg_config --pkglibdir)
-pg_installdir="$pg_libdir/baxdb"
+pg_installdir="$pg_libdir/pgwasdb"
 mkdir -p -m 755 "$pg_installdir" || {
   printf "Unable to create directory '$pg_installdir' for installation into PostgreSQL. Aborting.
 \n" 1>&2
@@ -71,7 +71,7 @@ mkdir -p -m 755 "$pg_installdir" || {
 (
   cd ./lib/tinyint-0.1.1 &&
     make &&
-    sed -i -e '1i\\\connect baxdb' -e 's|$libdir\/tinyint|$libdir/baxdb/tinyint|g' tinyint.sql &&
+    sed -i -e '1i\\\connect pgwasdb' -e 's|$libdir\/tinyint|$libdir/pgwasdb/tinyint|g' tinyint.sql &&
     cp tinyint.so "$pg_installdir" &&
     chmod -R 755 "$pg_installdir" &&
     cp tinyint.sql "$pg_installdir" # move the tinyint.sql into a location accessible by postgres user
@@ -83,29 +83,29 @@ cp ./ddl/setup.sql ./ddl/createtables.sql ./ddl/updatepermissions.sql "$pg_insta
 printf "Relocated $(ls ./ddl) to $pg_installdir.\n"
 
 sudo -u postgres psql -q -U postgres -f "$pg_installdir/setup.sql" || {
-  printf "Unable to perform setup for 'baxdb' database as user 'postgres'. Check UNIX account privileges and pg_hba.conf. Aborting.\n" 1>&2
+  printf "Unable to perform setup for 'pgwasdb' database as user 'postgres'. Check UNIX account privileges and pg_hba.conf. Aborting.\n" 1>&2
   exit 1
 }
 sudo -u postgres psql -q -U postgres -f "$pg_installdir/tinyint.sql" || {
-  printf "Unable to add 'tinyint' type to database 'baxdb'. Aborting.\n" 1>&2
+  printf "Unable to add 'tinyint' type to database 'pgwasdb'. Aborting.\n" 1>&2
   exit 1
 }
 sudo -u postgres psql -q -U postgres -f "$pg_installdir/createtables.sql" || {
-  printf "Unable to perform setup for 'baxdb' database as user 'postgres'. Check UNIX account privileges and pg_hba.conf. Aborting.\n" 1>&2
+  printf "Unable to perform setup for 'pgwasdb' database as user 'postgres'. Check UNIX account privileges and pg_hba.conf. Aborting.\n" 1>&2
   exit 1
 }
 sudo -u postgres psql -q -U postgres -f "$pg_installdir/updatepermissions.sql" || {
-  printf "Unable to perform setup for 'baxdb' database as user 'postgres'. Check UNIX account privileges and pg_hba.conf. Aborting.\n" 1>&2
+  printf "Unable to perform setup for 'pgwasdb' database as user 'postgres'. Check UNIX account privileges and pg_hba.conf. Aborting.\n" 1>&2
   exit 1
 }
-sed -i "1s/^/local baxdb baxdb_owner trust\n/" "$(sudo -u postgres psql -t -P "format=unaligned" -c "SHOW hba_file;")"
+sed -i "1s/^/local pgwasdb pgwasdb_owner trust\n/" "$(sudo -u postgres psql -t -P "format=unaligned" -c "SHOW hba_file;")"
 sudo -u postgres psql -t -P "format=unaligned" -c "SELECT pg_reload_conf();"
 
 # Remove the installation files from the database directory
 rm -f "$pg_installdir/setup.sql $pg_installdir/createtables.sql $pg_installdir/updatepermissions.sql"
-printf "Removed the temporary SQL files for building 'baxdb' database.\n"
+printf "Removed the temporary SQL files for building 'pgwasdb' database.\n"
 printf "Setup completed successfully.\n"
-printf "Please consider checking your 'pg_hba.conf' file to alter permissions to access the database. Permissions are currently set as 'local baxdb baxdb_owner trust'.\n"
+printf "Please consider checking your 'pg_hba.conf' file to alter permissions to access the database. Permissions are currently set as 'local pgwasdb pgwasdb_owner trust'.\n"
 
 # At this point, I was to be able to run
 # pip ./dml/setup.py install
